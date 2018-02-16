124303a4b4570d8b70fa5abc005205c173e46084
Ivan Yip <ivan_yip@msn.com>
2018-02-17 00:38:36 +0800

    basic function

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  nums: number[] = [0, 0, 0, 0];
+  interval: any;
+  tickTime: number = 5000;
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  start() {
+    if (!this.interval) {
+      this.run();
+    }
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.interval = null;
+    this.nums = [0,0,0,0];
+  }
+
+  program(time) {
+    this.start();
+    setTimeout(this.stop(), time);
+  }
+
+  onchange(e) {
+    this.tickTime = e.target.value;
+  }
+
+  private run() {
+    this.ranNum();
+    this.interval = setInterval(() => {
+      this.ranNum();
+    }, this.tickTime)
+  }
+
+  private ranNum() {
+    this.nums = this.nums.map(num => {
+      return Math.floor(Math.random() * 10)
+    })
+  }
 }
