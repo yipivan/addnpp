8682856a70d7f531e4d2d6b41dfa1fff21b59652
Ivan Yip <ivan_yip@msn.com>
2018-02-16 20:37:43 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { DisplayComponent } from './display/display.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
